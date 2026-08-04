e0bafcd2f7303209a6c3daede28d944314fbeefd
SEOBot <georgeglobalmarketing@gmail.com>
2026-08-04 16:27:02 +0300

    Sectiune de blog cu primele cinci articole
    - pagina /blog/ cu grila de articole si coloana laterala
    - cinci articole: costuri, apartament 2 camere, greseli frecvente,
      proiect tehnic 2D si iluminat interior
    - linkuri catre blog in meniu si footer, sitemap actualizat

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './design-interior-timisoara/index.html'],
+  content: ['./index.html', './design-interior-timisoara/index.html', './blog/index.html', './blog/*/index.html'],
   theme: {
     extend: {
       colors: {
